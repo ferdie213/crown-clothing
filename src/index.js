0944c91cbe457b7c54d8ad0781cd406deb56187c
Ferdinand Luter Gege <ferdinandfelix6@gmail.com>
2021-02-06 10:33:17 +0100

    moved the directory and shop data into its own reducer and created respective redux flow for directory and shop

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
+
 import './index.scss';
 import App from './App';
-import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
         <App />
-      </BrowserRouter>
-    </React.StrictMode>
+      </PersistGate>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
+
 
 
--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import { Route } from 'react-router-dom';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
+import CollectionPage from '../collection/collection';  
 
-        this.state ={
-            collections: SHOP_DATA
-        };
-    }
+const ShopPage = ({ match }) => (
+    <div>
+        <Route exact path={`${match.path}`} component= {CollectionsOverview } />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+);
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div>
-                {
-                    collections.map(({ id, ...otherCollectionProps}) => ((
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    )))
-                }
-            </div>
-        )
-    }
-}
-
-export default ShopPage;
+export default ShopPage; 
